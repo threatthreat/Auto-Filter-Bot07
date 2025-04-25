0e733847c120d3029dfd05a9dc1c353a5ea80bd3
threatthreat <grezy8007@gmail.com>
2025-04-25 07:35:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "quintessential-rosina-thread12-1ffd1ea5.koyeb.app/") #if heroku then paste the app link here ex: https://heroku......./
+URL = environ.get("URL", "http://quintessential-rosina-thread12-1ffd1ea5.koyeb.app/") #if heroku then paste the app link here ex: https://heroku......./
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
