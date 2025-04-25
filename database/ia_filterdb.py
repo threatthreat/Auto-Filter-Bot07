e0249ac263b2f9d757d436551a5776d3a2a3ee65
threatthreat <grezy8007@gmail.com>
2025-04-25 07:04:15 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -15,7 +15,7 @@ instance = Instance.from_db(db)
 
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute=True)
+    file_id = fields.StrField(attribute='_id', required=True)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
     caption = fields.StrField(allow_none=True)
