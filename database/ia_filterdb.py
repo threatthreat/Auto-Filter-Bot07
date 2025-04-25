018b5eb92458414c7c21984323bb39eb59af1d6d
threatthreat <grezy8007@gmail.com>
2025-04-25 06:52:31 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -68,76 +68,6 @@ async def get_search_results(query, max_results=MAX_BTN, offset=0, lang=None):
     cursor = Media.find(filter)
 
     # Sort by recent
-import logging
-from struct import pack
-import re
-import base64
-from pyrogram.file_id import FileId
-from pymongo.errors import DuplicateKeyError
-from umongo import Instance, Document, fields
-from motor.motor_asyncio import AsyncIOMotorClient
-from marshmallow.exceptions import ValidationError
-from info import DATABASE_URL, DATABASE_NAME, COLLECTION_NAME, MAX_BTN
-
-client = AsyncIOMotorClient(DATABASE_URL)
-db = client[DATABASE_NAME]
-instance = Instance.from_db(db)
-
-@instance.register
-class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    caption = fields.StrField(allow_none=True)
-
-    class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
-
-async def save_file(media):
-    """Save file in database"""
-
-    # TODO: Find better way to get same file_id for same media to avoid duplicates
-    file_id = unpack_new_file_id(media.file_id)
-    file_name = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(media.file_name))
-    file_caption = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(media.caption))
-    try:
-        file = Media(
-            file_id=file_id,
-            file_name=file_name,
-            file_size=media.file_size,
-            caption=file_caption
-        )
-    except ValidationError:
-        print(f'Saving Error - {file_name}')
-        return 'err'
-    else:
-        try:
-            await file.commit()
-        except DuplicateKeyError:      
-            print(f'Already Saved - {file_name}')
-            return 'dup'
-        else:
-            print(f'Saved - {file_name}')
-            return 'suc'
-
-async def get_search_results(query, max_results=MAX_BTN, offset=0, lang=None):
-    query = query.strip()
-    if not query:
-        raw_pattern = '.'
-    elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
-    else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]') 
-    try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        regex = query
-
-    filter = {'file_name': regex}
-    cursor = Media.find(filter)
-
-    # Sort by recent
     cursor.sort('$natural', -1)
 
     if lang:
@@ -208,4 +138,4 @@ def unpack_new_file_id(new_file_id):
             decoded.access_hash
         )
     )
-    return file_id￼Enter
+    return file_id
